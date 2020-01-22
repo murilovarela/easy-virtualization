2dbaad7f16439d5f7f20180556981f05716def17
Sergio Varela <sergio.varela@codeminer42.com>
2020-01-21 21:48:25 -0300

    Adds virtualization

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Category from './components/Category';
 import DATA_MOCK from './mocks/storeMock.json';
 import './App.css';
 
 function App() {
+  const [domCount, setDomCount] = useState(0);
+  const [isVirtualized, setIsVirtualized] = useState(false);
+
+  useEffect(()  =>  { 
+    const updateDomCount = () => {
+      setDomCount(document.getElementsByTagName('*').length);
+    };
+    
+    updateDomCount();
+    window.addEventListener('scroll',  updateDomCount);
+
+    return  ()  =>  {
+      window.removeEventListener('scroll',  updateDomCount);
+    };
+  },  [isVirtualized]);
+
   return (
     <div className="App">
-      {
-        DATA_MOCK.map(category => <Category key={category.id} {...category} />)
-      }      
+      {DATA_MOCK.map((category, index) => <Category key={category.id} {...category} virtualized={true} index={index} />)}
+      <div className="dom-count">
+        {`${domCount} dom-nodes`}
+        <button onClick={() => setIsVirtualized(!isVirtualized)} className="dom-count__btn">
+          {isVirtualized ? 'Disable' : 'Enable'}
+        </button>
+      </div>      
     </div>
   );
 }
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,15 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState, useRef, useMemo } from 'react';
 import Card from './Card';
 import './category.css';
 
-function Category({ title, description, items }) {
+function isVisible({ top, height, offset, index }) {
+  return (top  +  offset  +  height  >=  0  &&  top  -  offset  <=  window.innerHeight);
+}
+
+function Category({ title, description, items, index, virtualized }) {
+  const [isContainerVisible, setIsContainerVisible] = useState(index < 2);
+  const [isCardVisible, setIsCardVisible] = useState(items.map(() => index < 2));
+  const containerRef = useRef(null);
+  const cardRef = useRef([]);
+
+  useEffect(()  =>  { 
+    const isInViewport = () => {
+      const offset = 250;
+      if (containerRef.current) {
+        const { top, height } =  containerRef.current.getBoundingClientRect();
+        setIsContainerVisible(isVisible({ top, height, offset }));
+      } else {
+        setIsContainerVisible(false);
+      }
+  
+      const cardVisibility = items.map((_, itemIndex) => {
+        const card = cardRef.current[itemIndex];
+        if (card) {
+          const { top, height } = card.getBoundingClientRect();
+          return isVisible({ top, height, offset });
+        }
+        return index < 2;
+      });
+      setIsCardVisible(cardVisibility);
+    };
+    
+    // we run the function to measure the visibility as soon as the component mount
+    isInViewport();
+    window.addEventListener('scroll',  isInViewport);
+
+    return  ()  =>  {
+      window.removeEventListener('scroll',  isInViewport);
+    };
+  },  [index, items]);
+
+  const containerHeight = useMemo(()  =>  {
+    return Math.ceil(items.length  /  2  -  1)  *  30 + Math.ceil(items.length  /  2)  *  160;    
+    }, [items.length]);
+
   return (
     <div className="category">
       <div className="category__title">{title}</div>
       <div className="category__description">{description}</div>
-      <ul className="category__container">
-        {
-          items.map(item => <li key={`menu-item-${item.id}`}><Card {...item} /></li>)
+      <ul className="category__container" ref={containerRef} style={{ height: containerHeight }}>
+        {(!virtualized || isContainerVisible) &&
+          items.map((item, itemIndex) => (
+            <li style={{ height: 160 }} key={`menu-item-${item.id}`} ref={ref => { cardRef.current[itemIndex] = ref; }}>
+              {(!virtualized || isCardVisible) && <Card {...item} />}
+            </li>)
+          )
         }
       </ul>
     </div>
